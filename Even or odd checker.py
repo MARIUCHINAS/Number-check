270c2500f2bce078cff5088f7a7ab46aa621ff30
MARIUCHINAS <96793543+MARIUCHINAS@users.noreply.github.com>
2022-11-18 15:58:22 +0200

    Update Even or odd checker.py

--- Even or odd checker.py
+++ Even or odd checker.py
@@ -1,10 +1,12 @@
-number = input("Please enter a number: ")
+def main():
+    while True:
+        try:
+            number = int(input("Please enter a number: "))
+            if number % 2 == 0:
+                print("The number is even")
+            else:
+                print("The number is odd")
+        except ValueError:
+            print("That is not a number")
 
-if number.isdigit():
-    number = int(number)
-    if number % 2 == 0:
-        print("The number is even")
-    else:
-        print("The number is odd")
-else:
-    print("The number is not a number")
+main()
